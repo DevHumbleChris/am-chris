c5da445648ca46582f7849a75ceddc1cee72c527
DevHumbleChris <dev.humblechris254@gmail.com>
2021-07-15 23:35:26 +0300

    Commit Thur Jul 15 (@11:35pm)

--- data/Projects.js
+++ data/Projects.js
@@ -1,55 +1,56 @@
 const Projects = [
   {
-    name: 'Montflix',
+    name: 'Montflix.',
     technologiesUsed: [
       'NodeJS',
       'Express',
       'Pug',
-      'TMDB API'
+      'Restful API'
     ],
     imgPath: 'montflix.png',
     projectInfo: [
-      'Used Node; Express framework to create controllers that enabled in fetching data from the TMDB API (The Movie Database).',
+      'Used Node - Express framework to create controllers that enabled in fetching data from the TMDB API (The Movie Database).',
       'Used PUG template engine and Bootstrap to beautifully design the whole website.'
     ],
-    description: 'Monflix is web based app that keeps you with trend about mvoies and series.',
+    description: 'Monflix is web based app that keeps you with trend on movies and series.',
     githubLink: 'https://github.com/DevHumbleChris/montflix',
     webLink: 'https://montflix.herokuapp.com/'
   },
   {
-    name: 'Montana Foods',
+    name: 'Montana Foods.',
     technologiesUsed: [
       'VueJS',
       'Tailwind CSS',
       'Vuex',
-      'Vue Routes'
+      'Vue Routes',
+      'Restful API'
     ],
     imgPath: 'montana-foods.png',
     projectInfo: [
-      'Completely built with VueJS, Tailwind CSS, a bit of Vanilla CSS and (Edaman && The Meal DB) APIS was used as a source of data.',
-      'Vue Routes was used to create multiples views for the web application and vuex was used as centralised store for data'
+      'Completely built with VueJS, Tailwind CSS, a bit of Vanilla CSS and (Edaman && The Meal DB) APIS were used as a source of data.',
+      'Vue Routes was used to create multiples views for the web application and vuex was used to centrally store data.'
     ],
-    description: 'Montana-Foods is a web app that enables people to search different food recipes, watch YouTube Cooking Tutorials, give information about its equivalent food and much more!',
+    description: 'Montana-Foods is a web app that enables its users to search different food recipes, watch YouTube Cooking Tutorials, give information about its equivalent food and much more!',
     githubLink: 'https://github.com/DevHumbleChris/montana-foods',
     webLink: 'https://montana-foods.web.app/'
   },
   {
-    name: 'Rock, Papers And Scissors',
+    name: 'Rock, Papers And Scissors.',
     technologiesUsed: [
       'JavaScript',
       'Vanilla CSS'
     ],
     imgPath: 'rps-game.png',
     projectInfo: [
-      'Completely built with pure JavaScript (Vanilla) and CSS',
-      'Orientation for smaller display was put in place'
+      'Completely built with pure JavaScript (Vanilla) and CSS.',
+      'Orientation for smaller display was put in place.'
     ],
     description: 'Choose Rock and Scissors a number of times and you would definetly win.',
     githubLink: 'https://glitch.com/edit/#!/montana-rpsgame',
     webLink: 'https://montana-rpsgame.glitch.me/'
   },
   {
-    name: 'Vue Poll App',
+    name: 'Vue Poll App.',
     technologiesUsed: [
       'VueJS',
       'Vue Routes',
@@ -59,13 +60,63 @@ const Projects = [
     ],
     imgPath: 'vue-poll-app.png',
     projectInfo: [
-      'Poll app made with VueJS Framework and Tailwind CSS',
-      'Vuex was used to centralised data and vue routes was used to create vote (routes & views) and results (routes & views)',
-      'Results data visualization was possible through use of Vue-ChartJS package library'
+      'Poll app made with VueJS Framework and Tailwind CSS.',
+      'Vuex was used to centralised data and vue routes was used to create vote (routes & views) and results (routes & views).',
+      'Poll results data are beautifully visualized, this was possible through use of Vue-ChartJS package library.'
     ],
-    description: 'Tried to create my own poll app and its turned out great.',
+    description: 'Tried to create my own poll app and it turned out great. Definitely try it out!',
     githubLink: 'https://github.com/DevHumbleChris/vue-poll-app',
     webLink: 'https://vue-poll-app.web.app/'
+  },
+  {
+    name: 'Am Chris Portfolio.',
+    technologiesUsed: [
+      'VueJS',
+      'NuxtJS',
+      'Vuex',
+      'Tailwind CSS'
+    ],
+    imgPath: 'am-chris.png',
+    projectInfo: [
+      'Server Side Rendering and Progressive Web Application all made possible with NuxtJS.',
+      'The UI was all designed with Tailwind CSS.'
+    ],
+    description: 'Everything about me in one bundle of NuxtJS app (Portfolio).',
+    githubLink: 'https://github.com/DevHumbleChris/am-chris',
+    webLink: 'https://am-chris.herokuapp.com/'
+  },
+  {
+    name: 'GitHub API.',
+    technologiesUsed: [
+      'NodeJS',
+      'Express',
+      'Bootstrap',
+      'Pug',
+      'Restful API'
+    ],
+    imgPath: 'github-api.png',
+    projectInfo: [
+      'Express middlewares and Axios package library came in handy in getting and intregrating the GitHub Restful API.',
+      'Pug template engine and Bootstrap CSS were so musch of rescue into designing the User Interface.'
+    ],
+    description: 'Are you in GitHub? I can search you easily through this Express app.',
+    githubLink: 'https://github.com/DevHumbleChris/github-api',
+    webLink: 'https://am-chris-github-api.herokuapp.com/'
+  },
+  {
+    name: 'BlackJack Game.',
+    technologiesUsed: [
+      'JavaScript',
+      'Vanilla CSS'
+    ],
+    imgPath: 'github-api.png',
+    projectInfo: [
+      'Completely built with pure JavaScript (Vanilla) and CSS.',
+      'Orientation for smaller display was put in place.'
+    ],
+    description: 'If you hit twice and you get above 17, please stand. If I misled you make a new deal.',
+    githubLink: 'https://glitch.com/edit/#!/montana-blackjackgame',
+    webLink: 'https://montana-blackjackgame.glitch.me/'
   }
 ]
 
