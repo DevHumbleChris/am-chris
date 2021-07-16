5d77312eda037955c9fe5168a85f846ecbcbdbea
DevHumbleChris <dev.humblechris254@gmail.com>
2021-07-16 12:38:02 +0300

    Commit Fri Jul 16 (@12:37pm): Adding the bottom to Mobile Devices

---
+++ components/SideBottomNav.vue
@@ -0,0 +1,46 @@
+<template>
+  <div class="sideBottomNav fixed">
+    <div class="bottomNav">
+      <font-awesome-icon :icon="['far', 'newspaper']" />
+      <div>
+        Blog
+      </div>
+    </div>
+    <div class="bottomNav">
+      <font-awesome-icon :icon="['fas', 'user-astronaut']" />
+      <div>
+        Portfolio
+      </div>
+    </div>
+    <div class="bottomNav">
+      <font-awesome-icon :icon="['fab', 'whatsapp']" />
+      <div>
+        WhatsApp
+      </div>
+    </div>
+  </div>
+</template>
+
+<style>
+@media screen and (max-width: 640px) {
+  .sideBottomNav {
+    background: #f7f7f7;
+    bottom: 0;
+    left: 0;
+    right: 0;
+    display: flex;
+    flex-direction: row;
+    padding: 0.5rem;
+    justify-content: center;
+  }
+  .bottomNav {
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    padding: 0.2rem;
+    margin-left: 15px;
+    color: #8444d8;
+  }
+}
+</style>
--- layouts/default.vue
+++ layouts/default.vue
@@ -1,6 +1,7 @@
 <template>
-  <div>
+  <div class="relative">
     <Navbar />
     <Nuxt />
+    <SideBottomNav />
   </div>
 </template>
